fbbe4607b795322dd17d9cb52532e02828196579
Akanksha Patel <a.patel@easternenterprise.com>
2021-09-12 19:33:48 +0530

    test case for header created getByRole with title

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -1,14 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import Header from '../Header';
 
-it('should render same text passed into title prop', () => {
+it('should render same text passed into title prop getByText', () => {
   render(<Header title="My Header"/>);
   const headingElement = screen.getByText(/my header/i);
   expect(headingElement).toBeInTheDocument();
 });
 
-it('should render same text passed into title prop alt', () => {
+// it('should render same text passed into title prop getByRole', () => {
+//     render(<Header title="My Header"/>);
+//     const headingElement = screen.getByRole("heading");
+//     expect(headingElement).toBeInTheDocument();
+//   });
+
+  it('should render same text passed into title prop getByRole alt', () => {
     render(<Header title="My Header"/>);
-    const headingElement = screen.getByRole("heading");
+    const headingElement = screen.getByRole("heading", {name: "My Header"});
     expect(headingElement).toBeInTheDocument();
   });
